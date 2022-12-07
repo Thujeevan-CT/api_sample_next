47d81656565af21ec7edf1a18dbb0c262ba607f3
Thujeevan <thujeevan2@gmail.com>
2022-12-07 13:01:59 +0530

    post api created for testing

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,26 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ message: "Api sending successful" });
+  let data = {
+    name: "Joush",
+    email: "joush@mail.io",
+    message: "Make a Happy life!",
+  };
+
+  if (req.method === "GET") {
+    res.status(200).json({ data: data });
+  }
+
+  if (req.method === "POST") {
+    console.log("POST method is working!");
+    const { name, email, message } = req.body;
+
+    data = {
+      name: name,
+      email: email,
+      message: message,
+    };
+
+    res.status(200).json({ data });
+  }
 }
